8e0ebf0977cff04492c19efc56362209dbfa3300
Angelinacarvelli <angelinacarvellipro@gmail.com>
2026-09-18 12:03:16 +0200

    (Fix:GUI)fix static path

--- src/index.js
+++ src/index.js
@@ -11,9 +11,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+
 app.use(express.static(path.join(__dirname, '..', 'GUI')));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/GUI/Home_page.html');
+  res.sendFile(path.join(__dirname, '../GUI/Home_page.html'));
 });
 
 const loadRouter = (names) => {
@@ -43,7 +45,6 @@ const swaggerDocument = {
 };
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Route 404 (doit bien rester tout à la fin)
 app.use((req, res) => {
     res.status(404).json({ error: 'Route non trouvée' });
 });
