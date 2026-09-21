05432a12787ac76799c21a8e850941b0b951c0e6
Angelinacarvelli <angelinacarvellipro@gmail.com>
2026-09-21 14:27:39 +0200

    fix(gui): fix catalogue

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/recette/:slug', (req, res) => {
-  res.sendFile(path.join(__dirname, '../GUI/catalogue_exemple.html'));
+  res.sendFile(path.join(__dirname, '../GUI/catalogue.html'));
 });
 
 const loadRouter = (names) => {
